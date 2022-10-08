6ce8b6f665a43e63a10f0e0c720bb1a50df8dc0d
P129C61 <P129C61@L-R913XDNG>
2022-10-08 12:40:06 +0400

    added generics labsheet

---
+++ Labsheet2[Generics]/customStack/ICustomStack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customStack
+{
+    public interface ICustomStack<T>
+    {
+        void Push(T _data);
+        int Count();
+        T Pop();
+
+    }
+}
